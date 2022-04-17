6f5f82ff9c019207be78035784ea5ec8ddb1f341
royasulthani <99806068+royasulthani@users.noreply.github.com>
2022-04-17 09:30:47 +0400

    Add files via upload

---
+++ PRACTICE/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRACTICE
+{
+    class Admin:User
+    {
+        public bool  IsSuperAdmin { get; set; }
+        public string Section { get; set; }
+        public Admin(string username,int password,bool isSuperAdmin,string section):base(username,password)
+        {
+            IsSuperAdmin = isSuperAdmin;
+            Section = section;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Username:{Username} Password{Password} IsSuperAdmin{IsSuperAdmin} Section{Section}");
+        }
+
+    }
+}
---
+++ PRACTICE/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRACTICE
+{
+    class User
+    {
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if (value.Length > 6)
+                {
+                    _username = value;
+                }
+            }
+        }
+        public User(string username, int password)
+        {
+
+            {
+                Username = username;
+                Password = password;
+            }
+            {
+
+            }
+        }
+        private int _password;
+        public int Password
+        {
+            get => _password;
+            set
+            {
+                if (value > 0)
+                {
+                    _password = value;
+                }
+            }
+        }
+        
+   
+	{
+        
+        {      
+        
+	}  
+        
+    }
+    
+    public void ShowInfo();
+}
+
+
+   
+
