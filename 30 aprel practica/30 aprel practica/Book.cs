a5afc27dc8d134543ee58789f2f49d42a629026e
royasulthani <99806068+royasulthani@users.noreply.github.com>
2022-05-01 08:54:49 +0400

    Add files via upload

---
+++ 30 aprel practica/30 aprel practica/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30_aprel_practica
+{
+    class Book
+    {
+        public static int Counter =10;
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+        public string  Code { get; set; }
+       
+        public Book(string name,string authorname, int pagecount)
+        {
+            Counter++;
+            Name =name;
+            AuthorName = authorname;
+            PageCount = pagecount;
+            Code = Name.Substring(0, 2).ToUpper() + Counter;
+        }
+    }
+}
---
+++ 30 aprel practica/30 aprel practica/Library.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30_aprel_practica
+{
+    class Library
+    {
+        public List<Book> books = new List<Book>();
+        public List<Book> FindAllBooksByName(string value)
+        {
+            return books.FindAll(book => book.Name.Contains(value));
+        }
+        public void RemoveAllBooksByName(string value)
+        {
+            books.RemoveAll(Book => Book.Name.Contains(value));
+        }
+        public List<Book> SearchBooks(string value)
+        {
+            return books.FindAll(books.Name.Contains(value) || books.AuthorName.Contains() || books.PageCount.ToString().Contains(value));
+        }
+        public List<Book> FindAllBooksByPageCountRange(int min, int max)
+        {
+            return books.FindAll(Book => Book.PageCount >= min && Book.PageCount <= max);
+        }
+        public void RemoveBookByCode(String value)
+        {
+            Book WantedBook = books.Find(Book => Book.Code == value);
+            if (WantedBook != null)
+                books.Remove(WantedBook);
+        }
+    }
+}
+
