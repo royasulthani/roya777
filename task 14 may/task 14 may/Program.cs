5e9fd86aa2f13f7dff33f5e5fed4d61e202f9b1d
royasulthani <99806068+royasulthani@users.noreply.github.com>
2022-05-15 09:08:49 +0400

    Add files via upload

---
+++ task 14 may/task 14 may/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace task_14_may
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Silahin adini daxil edin");
+            string name = Console.ReadLine();
+            Console.WriteLine("Silahin capacity-ni daxil edin:");
+            int capacity;
+            do
+            {
+                capacity = Convert.ToInt32(Console.ReadLine());
+            } while (capacity<0);
+            Console.WriteLine("Silahin cari gulle sayini daxil edin");
+            int nowCap;
+            do
+            {
+                nowCap = Convert.ToInt32(Console.ReadLine());
+            } while (nowCap<0|| capacity<nowCap);
+            Weapon weapon1 = new Weapon(name, capacity, nowCap);
+            Console.WriteLine(weapon1.ShowInfo());
+            Console.WriteLine("Atis modu");
+            string ChangeFireMode = Console.ReadLine();
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    weapon1.Shoot();
+
+            }
+        }
+    }
+}
---
+++ task 14 may/task 14 may/Weapon.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_14_may
+{
+    class Weapon
+    {
+        public Weapon(string name,int capacity,int nowCap)
+        {
+            Id = Id++;
+            Name = name;
+            Capacity = capacity;
+            NowCap = nowCap;
+        }
+        private static int _id;
+        private int _capacity;
+        private int _nowCap;
+        public int Id { get; }
+        public string Name { get; set; }
+        public int NowCap { get; set; }
+        public int Capacity
+        {
+            get => _capacity;
+            set
+            {
+                if (value >= 0)
+                    _capacity = value;
+            }
+        }
+        public bool ChangeFireMode = false;
+        public int Count { get; set; }
+        public int Shoot(int count)
+        {
+            return --NowCap;
+        }
+        public int Fire()
+        {
+            NowCap = 0;
+            Console.WriteLine($"Gulle bitdi");
+            return 0;
+            {
+
+            }
+        }
+        public int GetRemainBulletCount()
+        {
+            return Capacity - NowCap;
+        }
+        public void Reload()
+        {
+            NowCap = 30;
+        }
+        public string ShowInfo()
+        {
+            return $"\n Weapon info:\nName:{Name}\nCapacity:{Capacity}\nNowCap:{NowCap}";
+        }
+        public void Check { get; set; }
+    }
+}
