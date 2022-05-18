352a8a31d05fcef6b3767f8bbded86a23064de43
royasulthani <99806068+royasulthani@users.noreply.github.com>
2022-05-18 07:58:59 -0700

    Add files via upload

---
+++ Practica1/js/main.js
@@ -0,0 +1,18 @@
+const box = document.querySelector(".row");
+
+fetch("https://restcountries.com/v3.1/all").then(response => response.json())
+.then(data => data.forEach(country =>{
+    box.innerHTML += `
+    <div class="col-md-3">
+     <div class="card" style="width: 18rem;">
+        <img src="${country.flags.png}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Country:${country.name.common}</h5>
+          <p class="card-text">Population:${country.population}</p>
+          <p class="card-region">Region:${country.region}</p>
+          <p class="card-capital">Capital:${country.capital}</p>
+        </div>
+      </div>
+    </div>
+    `
+}));
